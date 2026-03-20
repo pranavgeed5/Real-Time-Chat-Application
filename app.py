b4f5f522656834e5ed3d47dc2a5deef5df90eab3
Pranav Geed <pranavgeed5@gmail.com>
2026-03-20 14:42:57 +0530

    initial

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-# store messages
 messages = []
 
 @app.route('/')
 def index():
     return render_template('index.html', messages=messages)
 
-# receive message
 @socketio.on('message')
 def handle_message(data):
-    data['time'] = datetime.now().strftime("%I:%M %p")  # time
+    data['time'] = datetime.now().strftime("%I:%M %p")
     messages.append(data)
     send(data, broadcast=True)
 
